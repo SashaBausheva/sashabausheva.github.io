0d13f4110424d9e1656c03caaf05f5edddcc346f
Aleksandra Bausheva <aleksandrabausheva@Aleksandras-MacBook-Pro.local>
2025-02-17 17:51:20 -0500

    Cleanup

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -9,9 +9,7 @@ export const Skills = () => {
   return (
     <section id="skills" className="flex h-screen items-center justify-center">
       <div className="max-w-screen-lg text-center">
-        <h2 className="md:leading-10 py-8" style={{ fontSize: '1.5rem ' }}>
-          {t('skills_title')}
-        </h2>
+        <h2 className="xs:hidden md:inline-block md:leading-10 py-8">{t('skills_title')}</h2>
         <div className="grid xs:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3">
           <BrandIcons />
         </div>
